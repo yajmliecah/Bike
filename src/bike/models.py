0453c13ee09efca352813dcfec9029787b119254
mjasantome <michaeljay.santome@gmail.com>
2017-02-24 22:57:17 +0800

    added queryset and model managers

--- src/bike/models.py
+++ src/bike/models.py
@@ -48,6 +48,21 @@ class Edition(models.Model):
     def __unicode__(self):
         return self.name
     
+
+class ItemQuerySet(models.query.QuerySet):
+    
+    def negotiable(self):
+        return self.filter(negotiable=True)
+    
+
+class ItemManager(models.Manager):
+    
+    def get_queryset(self):
+        return ItemQuerySet(self.model, using=self._db)
+    
+    def negotiable(self):
+        return self.get_queryset().negotiable()
+    
     
 class Item(models.Model):
     CONDITION =(
@@ -92,6 +107,8 @@ class Item(models.Model):
     color_family = models.CharField(max_length=50, choices=COLOR_FAMILY, verbose_name=_("Color Family"))
     details = models.TextField(blank=True, null=True, verbose_name=_("Details"))
     submitted_on = models.DateField(auto_now=True, editable=False, verbose_name=_("Submitted on"))
+    
+    objects = ItemManager()
     
     class Meta:
         verbose_name = _("Item")
--- src/bike/views.py
+++ src/bike/views.py
@@ -12,3 +12,6 @@ class IndexView(TemplateView):
         return context
 
 
+    
+
+
